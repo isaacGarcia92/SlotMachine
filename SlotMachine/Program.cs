25d72434cb0f23192f1458f4e04d882de1bdf046
isaacGarcia92 <isaacvinicio.garcia@gmail.com>
2022-12-11 13:47:04 -0600

    Adding horizontal, vertical ans diagonal methods.

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -7,6 +7,20 @@ class Program
         // 2D Array
         int[,] slotMachineGrid = new int[3, 3];
 
+        // Introduction text and receiving user input
+        Console.WriteLine("Welcome to The Slot Machine");
+        Console.WriteLine("Please write what type of lines you want to play");
+        Console.WriteLine("Horizontal, Vertical or Diagonal");
+        string userOption = Console.ReadLine().ToLower();
+
+        // Checking if user option is invalid
+        while(userOption != "horizontal" && userOption != "vertical" && userOption != "diagonal")
+        {
+            Console.WriteLine("Invalid Option");
+            userOption = Console.ReadLine().ToLower();
+        }
+
+        // Starting slot machine
         Console.WriteLine("Type spacebar to start the slot machine or Enter to exit");
 
         while(true)
@@ -15,9 +29,19 @@ class Program
             if(key == ConsoleKey.Spacebar)
             {
                 PrintSlotMachine(slotMachineGrid);
-                IsMiddleRowTheSame(slotMachineGrid);
-            }
 
+                switch(userOption)
+                {
+                    case "horizontal": CheckHorizontalWin(slotMachineGrid);
+                        break;
+
+                    case "vertical": CheckVerticalWin(slotMachineGrid);
+                        break;
+
+                    case "diagonal": CheckDiagonalWin(slotMachineGrid);
+                        break;
+                }
+            }
             if(key == ConsoleKey.Enter)
             {
                 Console.WriteLine("Thanks for playing!");
@@ -41,18 +65,74 @@ class Program
         }
     }
 
-    public static void IsMiddleRowTheSame(int[,] array)
+    public static void CheckHorizontalWin(int[,] array)
     {
-        if ((array[1, 0] == array[1, 1]) && (array[1, 1] == array[1, 2]))
+        if ((array[0, 0] == array[0, 1]) && (array[0, 1] == array[0, 2]))
         {
-            Console.WriteLine("You won!");
+            Console.WriteLine("You won in top horizontal!");
             Console.WriteLine("Thanks for playing!");
             Environment.Exit(0);
         }
-
+        if((array[1, 0] == array[1, 1]) && (array[1, 1] == array[1, 2]))
+        {
+            Console.WriteLine("You won in middle horizontal!");
+            Console.WriteLine("Thanks for playing!");
+            Environment.Exit(0);
+        }
+        if ((array[2, 0] == array[2, 1]) && (array[2, 1] == array[2, 2]))
+        {
+            Console.WriteLine("You won in bottom horizontal!");
+            Console.WriteLine("Thanks for playing!");
+            Environment.Exit(0);
+        }
         else
         {
-            Console.WriteLine("You lost!");
+            Console.WriteLine("Try again with spacebar to keep playing!");
+        }
+    }
+
+    public static void CheckVerticalWin(int[,] array)
+    {
+        if ((array[0, 0] == array[1, 0]) && (array[1, 0] == array[2, 0]))
+        {
+            Console.WriteLine("You won in left vertical!");
+            Console.WriteLine("Thanks for playing!");
+            Environment.Exit(0);
+        }
+        if ((array[0, 1] == array[1, 1]) && (array[1, 1] == array[2, 1]))
+        {
+            Console.WriteLine("You won in middle vertical!");
+            Console.WriteLine("Thanks for playing!");
+            Environment.Exit(0);
+        }
+        if ((array[0, 2] == array[1, 2]) && (array[1, 2] == array[2, 2]))
+        {
+            Console.WriteLine("You won in right vertical!");
+            Console.WriteLine("Thanks for playing!");
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine("Try again with spacebar to keep playing!");
+        }
+    }
+
+    public static void CheckDiagonalWin(int[,] array)
+    {
+        if ((array[0, 0] == array[1, 1]) && (array[1, 1] == array[2, 2]))
+        {
+            Console.WriteLine("You won in left diagonal!");
+            Console.WriteLine("Thanks for playing!");
+            Environment.Exit(0);
+        }
+        if ((array[0, 2] == array[1, 1]) && (array[1, 1] == array[2, 0]))
+        {
+            Console.WriteLine("You won in middle vertical!");
+            Console.WriteLine("Thanks for playing!");
+            Environment.Exit(0);
+        }
+        else
+        {
             Console.WriteLine("Try again with spacebar to keep playing!");
         }
     }
